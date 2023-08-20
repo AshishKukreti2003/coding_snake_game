dcbbc27f61fcc914c79bc4ec0c18411325e8ede8
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-08-20 19:54:47 +0530

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // Game Constants & variables
 let inputDir = { x: 0, y: 0 };
-const foodSoung = new Audio("music/food.mp3");
+const foodSound = new Audio("music/food.mp3");
 const gameOverSound = new Audio("music/gameover.mp3");
 const moveSound = new Audio("music/move.mp3");
 const musicSound = new Audio("music/music.mp3");
-let speed = 2;
+let speed = 5;
 let lastPaintTime = 0;
 let snakeArr = [{ x: 13, y: 15 }];
 food = { x: 6, y: 7 };
@@ -21,8 +21,16 @@ function main(ctime) {
   gameEngine();
 }
 
-function isCollide(sarr){
-  return false;
+function isCollide(snake){
+  // if you bump into yourself
+  for (let i = 1; i < snakeArr.length; i++) {
+    if(snake[i].x ===snake[0].x && snake[i].y===snake[0].y){
+      return true;
+    }
+  }
+  // if you bump into the wall
+  if(snake[0].x>=18 || snake[0].x<=0 || snake[0].y>=18 || snake[0].y<=0)
+  return true;
 }
 
 function gameEngine() {
@@ -39,11 +47,28 @@ function gameEngine() {
 
   // if you have eaten the food, increment the score and regenerate the food
   if(snakeArr[0].y==food.y && snakeArr[0].x==food.x){
-    snakeBody.unshift({x:snakeArr[0].x + inputDir.x, y:snakeArr[0].y + inputDir.y});
+    foodSound.play();
+    score+=1;
+    if(score>hiscoreval){
+      hiscoreval=score;
+      localStorage.setItem("hiscore",JSON.stringify(hiscoreval));
+      hiscoreBox.innerHTML = "Hi Score: "+ hiscoreval;
+    }
+    scoreBox.innerHTML = "Score: "+score;
+    snakeArr.unshift({x:snakeArr[0].x + inputDir.x, y:snakeArr[0].y + inputDir.y});
     let a = 2;
     let b = 16;
     food={x: Math.round(a+(b-a)*Math.random()), y: Math.round(a+(b-a)*Math.random())}
   }
+
+  // moving the snake
+  for (let i = snakeArr.length - 2; i>=0; i--) {''
+    snakeArr[i+1] = {...snakeArr[i]};
+  }
+
+  snakeArr[0].x += inputDir.x;
+  snakeArr[0].y += inputDir.y;
+
 
   // part 2: display  the snake
   // display the snake
@@ -69,6 +94,16 @@ function gameEngine() {
 }
 
 // main logic starts here
+musicSound.play();
+let hiscore = localStorage.getItem("hiscore");
+if(hiscore==null){
+  hiscoreval=0;
+  localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
+}
+else{
+  hiscoreval=JSON.parse(hiscore);
+  hiscoreBox.innerHTML = "Hi Score: "+ hiscore;
+}
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e => {
   inputDir = { x: 0, y: 0 }; //start the game
